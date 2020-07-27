4049472ab19da7d517cfbdc466be928a5d7eb72a
DanielDeverell <daniel@getperksy.com>
2020-07-27 12:40:43 -0400

    add books page

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql } from 'gatsby';
+import useSiteMetadata from '../hooks/use-sitemetadata';
+import Layout from '../components/layout';
+import styles from './blog.module.css';
 
-export default () => (
-  <>
-    <h1>Books</h1>
-    <Link to="/">Home</Link>
-  </>
-);
+const Books = ({ location, data }) => {
+  const { title, description } = useSiteMetadata();
+  const books = data.allContentfulBook.edges;
+  console.log(' ', books);
+  return (
+    <Layout location={location}>
+      <div style={{ background: '#fff' }}>
+        <div className={styles.hero}>Books</div>
+        <div className="wrapper">
+          <h2 className="section-headline">Publications</h2>
+          <ul className="article-list">
+            {books.map(({ node }) => {
+              return (
+                <li key={node.slug}>
+                  <h2>{node.title}</h2>
+                  <p>{node.yearOfPublication}</p>
+                  <p>{node.body.body}</p>
+                  {/* <ArticlePreview article={node} /> */}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query AllBooks {
+    allContentfulBook {
+      edges {
+        node {
+          title
+          yearOfPublication
+          slug
+          body {
+            body
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Books;
